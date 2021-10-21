3ea54b03bd429aad99ea69b5b70f5ed4ef53d8e2
Araviel-IO <contact@araviel.io>
2021-10-21 18:24:19 +0200

    added safecoin logo for balance list

--- src/components/TokenIcon.js
+++ src/components/TokenIcon.js
@@ -4,7 +4,7 @@ export default function TokenIcon({ mint, url, tokenName, size = 20, ...props })
   const [hasError, setHasError] = useState(false);
 
   if (!url && mint === null) {
-    url = 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/solana/info/logo.png';
+    url = 'https://github.com/araviel-io/CodeSnippets/blob/main/safecurnew.jpg?raw=true';
   }
 
   if (hasError || !url) {
